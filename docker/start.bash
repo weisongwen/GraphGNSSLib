3e79c51155bf432908a19b1820910263b7709b7a
Yihan Zhong <68424083+Pirkaklo@users.noreply.github.com>
2022-06-01 13:31:19 +0800

    Update start.bash

--- docker/start.bash
+++ docker/start.bash
@@ -14,6 +14,6 @@ else
 	    mkdir $SHARED_HOST_DIR
 	fi
 	#docker run --gpus all -it -v /home/hrz/project/ros/melodic/LvisamTest:/root/LvisamTest  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE -e ROS_MASTER_URI=http://172.17.0.2:8899 -e ROS_HOSTNAME=172.17.0.2 --cpus 0.5 --cpuset-cpus=2 --name melodic melodic-gpu /bin/bash 
-	docker run -it   -v /tmp/.X11-unix:/tmp/.X11-unix -v $SHARED_HOST_DIR:$SHARED_DOCKER_DIR  -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE -e ROS_MASTER_URI=http://172.17.0.2:8899 -e ROS_HOSTNAME=172.17.0.2 --name graph1 ros:GraphGNSSLib /bin/bash 
+	docker run -it   -v /tmp/.X11-unix:/tmp/.X11-unix -v $SHARED_HOST_DIR:$SHARED_DOCKER_DIR  -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE -e ROS_MASTER_URI=http://172.17.0.2:11311 -e ROS_HOSTNAME=172.17.0.2 --name graph1 ros:GraphGNSSLib /bin/bash 
 	sudo chmod 777 $SHARED_HOST_DIR
 fi
